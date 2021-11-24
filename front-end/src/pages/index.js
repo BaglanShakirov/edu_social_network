a562e380a55111494e40ddd761ca4d65038f39b2
YernarT <toktaryernar@gmail.com>
2021-11-24 16:32:17 +0600

    Добавьте стр.: home page, search page и т. Д.

---
+++ front-end/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as HomePage } from './HomePage';
+export { default as ProfilePage } from './ProfilePage';
+export { default as SearchPage } from './SearchPage';
+
+export { default as LoginPage } from './LoginPage';
+export { default as RegisterPage } from './RegisterPage';
