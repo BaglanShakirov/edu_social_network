8fa122e9537e23d79fbf2558fde8c20985201811
YernarT <toktaryernar@gmail.com>
2021-11-24 16:30:30 +0600

    Инициализировать маршрутизацию

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import {
+	HomePage,
+	ProfilePage,
+	SearchPage,
+	RegisterPage,
+	LoginPage,
+} from './pages';
+
+import './customize.css';
 
 export default function App() {
-	return <div>Hello World!</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/profile" element={<ProfilePage />} />
+				<Route path="/search" element={<SearchPage />} />
+
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
